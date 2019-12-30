382795cb3f520703c5db2badfca68e4b0589a288
Pranay Garg <pranaygarg2013@gmail.com>
2019-12-30 17:34:49 +0530

    modified the vuln a little

--- vuln.c
+++ vuln.c
@@ -1,14 +1,34 @@
+/****************************
+#
+# Pranay Garg
+#
+# Test code for buff_over
+#   - Added comments
+# adapted from:
+# https://samsclass.info/127/proj/ED402.htm
+#
+*****************************/
 #include <stdio.h>
 #include <unistd.h>
 
+/*
+ * Vulnerable function with an overflow problem.
+ */
 int vuln() {
     char buf[400];
     int r;
+    /* Some assembly code to print the stack start address! */
     register int i asm("rsp");
     printf("Welcome to the Proj 13 Server!\n\n");
     printf("$rsp = %#018x\n\nEnter some text:\n", i);
     r = read(0, buf, 800);
     printf("You said: %s\n", buf);
+    return 0;
+}
+
+/* A function that's never called! */
+int pandu_rocks(){
+    printf("We won!\n");
     return 0;
 }
 
